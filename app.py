c357c4fde6b4022b2114c3e55f39c4adbe791757
Bryce Laxson <blaxson@calpoly.edu>
2020-04-29 21:26:01 -0700

    Added initial home, journal, and mood html templates; created initial pages for these as well

--- app.py
+++ app.py
@@ -1,10 +1,20 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route("/")
+@app.route("/home")
 def home():
-    return "Hello World!"
+    return render_template("home.html")
 
+@app.route("/journal")
+def journal():
+    return render_template("journal.html")
+
+@app.route("/mood")
+def mood():
+    return render_template("mood.html")
+    
 if __name__ == "__main__":
-    app.run()
+    #app.run() # when not debugging
+    app.run(debug = True) # when debugging
