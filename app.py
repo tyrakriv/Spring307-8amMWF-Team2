8ae5903509b9fccaf2768243076e3e05683101a8
Bryce Laxson <blaxson@calpoly.edu>
2020-04-30 00:48:09 -0700

    Added an initial login page and login template

--- app.py
+++ app.py
@@ -5,15 +5,19 @@ app = Flask(__name__)
 @app.route("/")
 @app.route("/home")
 def home():
-    return render_template("home.html")
+    return render_template("home.html", title = "Home")
 
 @app.route("/journal")
 def journal():
-    return render_template("journal.html")
+    return render_template("journal.html", title = "Journal")
 
 @app.route("/mood")
 def mood():
-    return render_template("mood.html")
+    return render_template("mood.html", title = "Mood")
+
+@app.route("/login")
+def login():
+    return render_template("login.html", title = "Login")
     
 if __name__ == "__main__":
     #app.run() # when not debugging
