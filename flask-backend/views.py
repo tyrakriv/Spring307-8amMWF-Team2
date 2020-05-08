d99dca804cac2a90468a39d0f1c1752cd4d1f4fe
Bryce Laxson <blaxson@calpoly.edu>
2020-05-07 18:53:25 -0700

    bug fix. Fixed last_name input bug

--- flask-backend/views.py
+++ flask-backend/views.py
@@ -17,7 +17,7 @@ def register_user():
     user_data = request.get_json()
     # user = User.query
     passwd_hash = generate_password_hash(user_data['password'])
-    new_user = User(email=user_data['email'], username=user_data['username'], first_name=user_data['first_name'], last_name=['last_name'], password_hash=passwd_hash, is_admin=True)
+    new_user = User(email=user_data['email'], username=user_data['username'], first_name=user_data['first_name'], last_name=user_data['last_name'], password_hash=passwd_hash)
 
     db.session.add(new_user)
     db.session.commit()
