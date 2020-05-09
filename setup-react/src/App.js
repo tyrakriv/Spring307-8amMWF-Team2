390642c111766593db5030b4c6854512bf4d92b0
ameliabruscia <aibrusci@calpoly.edu>
2020-05-09 11:46:50 -0700

    #30

--- setup-react/src/App.js
+++ setup-react/src/App.js
@@ -1,16 +1,25 @@
 import React, {useEffect, useState, Component} from 'react';
 import './App.css';
+import {newUserInfo} from './components/newUserInfo';
 import {Login} from './components/Login';
 import {Register} from './components/Register';
 import {Container} from "semantic-ui-react"
 
 function App(){
-
+    const [newUser, setNewUser] = useState([]);
+    useEffect(() => {
+      fetch('/getNewestUser').then(response => 
+        response.json().then(data => {
+          setNewUser(data.newestUser);   
+        })
+      );
+    }, [])
     return (
       <div className="login-form">
         <Container>
-          <Login/>
+          <Register/>
         </Container>
+        <newUserInfo newUser = {newUser}/>
       </div>
 
     );
