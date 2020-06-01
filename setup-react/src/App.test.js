6120276e22ad8f9702a5f060b2bed4fd4888388d
tyrakriv <tytykr@gmail.com>
2020-05-31 18:19:18 -0700

    implementing travis ci

--- setup-react/src/App.test.js
+++ setup-react/src/App.test.js
@@ -1,15 +1,17 @@
+// import React from 'react';
+// import ReactDOM from 'react-dom';
+// import App from './App'
+// import { render } from '@testing-library/react';
+// import {shallow} from 'enzyme';
+
+// it('renders without crashing', () => {
+//   const div = document.createElement('div');
+//   ReactDOM.render(<App />, div);
+  //console.log('true')
+  //ReactDOM.unmountComponentAtNode(div);
+// });
+
 import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App'
-
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  console.log('true')
-  ReactDOM.unmountComponentAtNode(div);
-});
-
-/*import React from 'react';
 import { render } from '@testing-library/react';
 import App from './App';
 
@@ -17,4 +19,4 @@ test('renders learn react link', () => {
   const { getByText } = render(<App />);
   const linkElement = getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
-});*/
+});
