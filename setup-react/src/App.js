c9cbaa63968b66e436b01f8d316b4af2b5993b28
tyrakriv <tytykr@gmail.com>
2020-06-04 20:11:07 -0700

    Survey format

--- setup-react/src/App.js
+++ setup-react/src/App.js
@@ -1,4 +1,3 @@
-
 import React, {useEffect, useState, Component} from 'react';
 import './App.css';
 import {NewUserInfo} from './components/NewUserInfo';
@@ -13,25 +12,24 @@ import Tabs from './components/Tabs';
 import Journal from './components/Journal';
 import Survey from './components/Survey';
 import Profile from './components/Profile';
-
+import Todos from './components/Todos';
+import Quote from './components/Quote';
+import QuotePage from './components/QuotePage';
 
 function App(){
     return (
-      <div>
-        <div className="login-form">
-          <Switch>
-            <Route exact path="/" component={Login}/>
-            <Route path="/register" component={Register}/>
-            <Route path="/homepage" component={Homepage}/>
-            <Route path="/profile" component={Profile}/>
-            <Route path="/journal/entry" component={JournalForm}/>
-            <Route path="/journal" component={Journal}/>
-            <Route path="/survey" component={Survey}/>
-          </Switch>
-        </div>
+      <div className="login-form">
+        <Switch>
+          <Route exact path="/" component={Login}/>
+          <Route path="/register" component={Register}/>
+          <Route path="/homepage" component={Homepage}/>
+          <Route path="/profile" component={Profile}/>
+          <Route path="/journal/entry" component={JournalForm}/>
+          <Route path="/journal" component={Journal}/>
+          <Route path="/survey" component={Survey}/>
+          <Route path="/Checklist" component={Todos}/>
+        </Switch>
       </div>
 
     );
 }
-
-export default App;
